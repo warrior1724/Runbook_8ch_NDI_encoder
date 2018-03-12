edb24281a5a48f44f605c4d4d5d4ce56159d215e
olzzon <github@olzzon.dk>
2018-03-12 09:40:06 +0100

    Added comments when script is running (echo)

--- runbook.sh
+++ runbook.sh
@@ -6,12 +6,14 @@
 InstallDir=$PWD
 
 #Cleanup Ubuntu:
+echo "Cleanup Ubuntu"
 sudo apt-get -y purge account-plugin-aim account-plugin-facebook account-plugin-flickr account-plugin-jabber account-plugin-salut account-plugin-yahoo aisleriot gnome-mahjongg gnome-mines gnome-sudoku unity-lens-photos unity-lens-video unity-scope-audacious unity-scope-chromiumbookmarks unity-scope-clementine unity-scope-colourlovers unity-scope-devhelp unity-scope-firefoxbookmarks unity-scope-gmusicbrowser unity-scope-gourmet unity-scope-musique unity-scope-openclipart unity-scope-texdoc unity-scope-tomboy unity-scope-video-remote unity-scope-zotero unity-webapps-common libreoffice*  thunderbird* rhythm*
 sudo apt-get update
 sudo apt-get -y upgrade
 
 #Resize Ubuntu network system buffer: 
 #(to avoid h264 blur and other network related problems)
+echo "Resize Ubuntu network system buffer"
 sudo sysctl -w net.core.rmem_max=8388608
 sudo sysctl -w net.core.wmem_max=8388608
 sudo sysctl -w net.core.rmem_default=65536
@@ -22,10 +24,12 @@ sudo sysctl -w net.ipv4.tcp_mem='8388608 8388608 8388608'
 sudo sysctl -w net.ipv4.route.flush=1
 
 #Dependencies for FFmpeg:
+echo "Dependencies for FFmpeg"
 sudo apt-get update -qq
 sudo apt-get -y install autoconf automake build-essential cmake git libass-dev libfreetype6-dev libsdl2-dev libtheora-dev libtool libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev libxcb-xfixes0-dev mercurial pkg-config texinfo wget zlib1g-dev yasm libx264-dev libx265-dev libvpx-dev libfdk-aac-dev libmp3lame-dev libopus-dev libopencore-amrnb-dev libopencore-amrwb-dev librtmp-dev 
 
 #NDI SDK:
+echo "NDI SDK"
 cd $InstallDir/NDI-SDK
 sudo chmod +x InstallNDISDK_v3_Linux.sh
 sudo ./InstallNDISDK_v3_Linux.sh
@@ -39,18 +43,22 @@ mv  "$InstallDir/NDI-SDK/NDI SDK for Linux/lib/x86_64-linux-gnu" ./lib
 cd $HOME
 
 #Move and activate NDI configfile: 
+echo "Move and activate NDI configfile"
 sudo mv $InstallDir/NDI-SDK/NDI.conf /etc/ld.so.conf.d/NDI.conf
 sudo ldconfig
 
 #Decklink SDK:
+echo "Move Decklink SDK"
 mkdir $HOME/DecklinkSDK
 mv $InstallDir/DecklinkSDK/Linux $HOME/DecklinkSDK
 
 #Get FFmpeg:
+echo "Get FFmpeg"
 cd $HOME
 git clone https://git.ffmpeg.org/ffmpeg.git ffmpeg
 
 #Compile:
+echo "Compile FFmpeg"
 mkdir $HOME/ffmpeg_build
 mkdir $HOME/ffmpeg_build/lib 
 mkdir $HOME/ffmpeg_build/lib/pkgconfig
@@ -63,18 +71,23 @@ make
 make install
 
 #Move Startupscripts to folder:
+echo "Move startupscripts"
 mkdir $HOME/runffmpeg
-mv $InstallDir/StartupScripts/encode8HDSDI.sh $HOME/runffmpeg/encode8HDSDI.sh
-mv $InstallDir/StartupScripts/HDSDItoNDI.sh $HOME/runffmpeg/HDSDItoNDI.sh
+mv $InstallDir/StartUpScripts/encode8HDSDI.sh $HOME/runffmpeg/encode8HDSDI.sh
+mv $InstallDir/StartUpScripts/HDSDItoNDI.sh $HOME/runffmpeg/HDSDItoNDI.sh
 
 
 #Load Encoderscript at startup:
+echo "Set Encoderscript at startup"
 sudo chmod +X HDSDItoNDI.sh
 sudo chmod +X encode8HDSDI.sh
-sudo mv $InstallDir/encode8HDSDI.sh.desktop ~/.config/autostart/encode8HDSDI.sh.desktop
+sudo mv $InstallDir/StartUpScripts/encode8HDSDI.sh.desktop ~/.config/autostart/encode8HDSDI.sh.desktop
 
 #Install Decklink GUI:
+echo "Install Decklink GUI - desktopvideo"
 cd $InstallDir/DecklinkGUI
 sudo apt-get -y install ./desktopvideo*
 
+#Finished:
+echo "Manually set Decklink card for 8 inputs in Desktopvideo"
 
